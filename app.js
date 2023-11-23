2916757254abc438f110be877723fc20280a84a2
Dejorden <d.moerman@hotmail.nl>
2023-11-23 14:07:15 +0100

    write a response to request

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const http = require('http');
 
-http.createServer((req, res) => {
-    console.log(req);;
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.headers, req.method);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>NODE.JS!!</title></head>');
+    res.write('<body><h1>Hallo Wereld!!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
+
+server.listen(3000);
